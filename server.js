181971ed3dffc5bef7dcb106e2dd131ef9c06235
Humma Noshine <Hummanoshine@gmail.com>
2022-04-12 21:01:57 +0100

    images

--- server.js
+++ server.js
@@ -4,14 +4,44 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const multer = require("multer");
+const upload = multer({dest: "uploads/"})
+const fs =require("fs")
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const { uploadFile, getFileStream } = require('./s3')
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 // handlebars stuff
 const hbs = exphbs.create({ helpers });
+
+// s3
+
+
+app.get('/images/:key', (req, res) => {
+  console.log(req.params)
+  const key = req.params.key
+  const readStream = getFileStream(key)
+
+  readStream.pipe(res)
+})
+
+app.post("/images", upload.single("image"), (req,res) => {
+  const file =req.file
+  console.log(file)
+  const description = req.body.description
+  res.send("Yayy")
+})
+
+const result = await uploadFile(file)
+  await unlinkFile(file.path)
+  console.log(result)
+  const description = req.body.description
+  res.send({imagePath: `/images/${result.Key}`})
+
 
 const sess = {
   secret: 'Super secret secret',
