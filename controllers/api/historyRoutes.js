10421db12afae475821ff8c24dcbbe302a53584f
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-04-14 18:44:20 +0100

    re.session log in before viewing history

--- controllers/api/historyRoutes.js
+++ controllers/api/historyRoutes.js
@@ -1,3 +1,9 @@
 const router = require("express").Router();
 const User = require("../../models");
 const { ExerciseRecord } = require("../../models");
+outer.get("/", async (req, res) => {
+  try {
+    // if (!req.session.logged_in) {
+    //   res.status(400).json({ message: "You are not logged in." });
+    //   return;
+    // }
