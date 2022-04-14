6aa910c38d842ec25579993563fc4ea1dfeed98c
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-04-14 21:32:19 +0100

    working histor

--- controllers/api/historyRoutes.js
+++ controllers/api/historyRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const User = require("../../models");
 const { ExerciseRecord } = require("../../models");
 
-router.get("/", async (req, res) => {
+router.get("/history", async (req, res) => {
   res.send("hello");
 });
 // if (!req.session.logged_in) {
